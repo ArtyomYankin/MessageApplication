59eef63785ef5018b4c810e095d8e0dac6e899e1
ArtyomYankin <a.yankin@itransition.com>
2021-10-15 03:07:17 +0300

    Changed sql expression

--- MA.Repository/UserRepo/UserRepository.cs
+++ MA.Repository/UserRepo/UserRepository.cs
@@ -28,35 +28,34 @@ namespace MA.Repository
 
         public User Get(T entity)
         {
+            string sqlExpression = $"SELECT * FROM User WHERE Email=\"{entity.Email}\" AND Password=\"{entity.Password}\"";
+            User user = new User();
             using (var connection = new SqliteConnection($"Data Source={DbPath}"))
             {
                 connection.Open();
 
-                SqliteCommand commandEmail = new SqliteCommand();
-
-                SqliteCommand commandPassword = new SqliteCommand();
-
-                SqliteCommand commandId = new SqliteCommand();
-                commandId.Connection = connection;
-                commandPassword.Connection = connection;
-                commandEmail.Connection = connection;
-                commandId.CommandText = $"SELECT Id FROM User WHERE Email=\"{entity.Email}\" AND Password=\"{entity.Password}\"";
-                commandEmail.CommandText = $"SELECT Email FROM User WHERE Email=\"{entity.Email}\" AND Password=\"{entity.Password}\"";
-                commandPassword.CommandText = $"SELECT Password FROM User WHERE Email=\"{entity.Email}\" AND Password=\"{entity.Password}\"";
-                int UserId = Convert.ToInt32(commandId.ExecuteScalar());
-                string UserEmail = (string)commandEmail.ExecuteScalar();
-                string UserPassword = (string)commandPassword.ExecuteScalar();
-                var user = new User()
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
                 {
-                    Id = UserId,
-                    Email = UserEmail,
-                    Password = UserPassword
-                };
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            user = new User()
+                            {
+                                Id = reader.GetInt32(0),
+                                Email = reader.GetString(1),
+                                Password = reader.GetString(2)
+                            };
+                        }
+                    }
+                }
 
-                return user;
             }
-
+            return user;
         }
+
+
 
         public async Task<User> GetById(string id)
         {
@@ -82,7 +81,7 @@ namespace MA.Repository
                         }
                     }
                 }
-                
+
             }
             return user;
         }
